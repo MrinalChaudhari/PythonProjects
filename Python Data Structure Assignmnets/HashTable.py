e82c0aa4f9dd6a1fab504e4465f1a0e8b126a656
MrinalChaudhari <mission999c@gmail.com>
2022-01-29 17:12:58 -0500

    new file added

---
+++ Python Data Structure Assignmnets/HashTable.py
@@ -0,0 +1,11 @@
+"""what is hash table?
+
+It is also known as hash map is a data structure used to implement an associative array.It is a structure that can map keys to values."""
+"""How it works?
+
+A hash table uses a hash function to compute an index into an array of buckets or slots, from which the correct value can be found."""
+"""In cases where I have a key for each element and I don't know the index of the element into an array, hashtables perform better than arrays (O(1) vs O(n)). The hash table search performs O(1) in the average case"""
+
+""" Hash function creates the ASCII value for its key and stored the element in that ascii index."""
+"""inear probing is a scheme in computer programming for resolving collisions in hash tables, data structures for maintaining a collection of key–value pairs and looking up the value associated with a given key"""
+
